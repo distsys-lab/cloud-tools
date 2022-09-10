cb90801d2c0f55ded40d861a517d1cbdb697c984
koya <kouyan120706@gmail.com>
2022-09-10 20:18:14 +0900

    import aws-tools

---
+++ aws-tools/list_all_elastic_ips.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set -x
+
+IFS='
+'
+
+regions=`cat config.txt | grep '^region_list' | cut -d '=' -f 2-`
+
+for i in `cat $regions`
+do
+	region=`echo $i | cut -d : -f 1`
+	name=`echo $i | cut -d : -f 3`
+
+	echo -n $name=
+	aws --region $region ec2 describe-addresses | jq -r ".Addresses[].PublicIp"
+done
