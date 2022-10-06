bbb1b8a14d6555be3ebc7e0bf51e8ab8add1b5ef
Koya Shiozaki <67150414+skoya76@users.noreply.github.com>
2022-10-06 11:02:32 +0900

    Update create_instance.sh

--- gcp-tools/create_instance.sh
+++ gcp-tools/create_instance.sh
@@ -2,4 +2,4 @@
 
 source ./config.txt
 
-gcloud compute instances create "$1" --zone $2 --image $image_type --machine-type $3 --metadata=ssh-keys=ubuntu:~/.ssh/id_rsa.pub
+gcloud compute instances create "$1" --zone $2 --image $image_type --machine-type $3
