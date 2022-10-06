69cd1980175f3663b3456c90c49d6122fc8cdd5b
koya <kouyan120706@gmail.com>
2022-10-06 11:56:23 +0900

    ubdate vm nameing

--- cross-exp/merge_each_cloud_hosts.sh
+++ cross-exp/merge_each_cloud_hosts.sh
@@ -3,15 +3,15 @@ cd ..
 
 cat aws-tools/my-hosts-list.txt | sed s/=/:/g | while read line
 do
-	echo "ubuntu:$line"
+	echo "ubuntu:aws-$line"
 done
 
 tail -n +4 azure-tools/vm.ip | sed -e 's/  */:/g' | cut -d ':' -f 1,2 | while read line
 do
-	echo "ubuntu:$line"
+	echo "ubuntu:azure-$line"
 done
 
 cat gcp-tools/vm.ip | while read line
 do
-	echo "$USER:$line"
+	echo "$USER:gcp-$line"
 done
