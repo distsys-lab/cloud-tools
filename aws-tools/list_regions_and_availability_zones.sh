74c4cc3c4173324eeabba23b23ee4ee11a32bc79
koya <kouyan120706@gmail.com>
2022-10-10 14:45:17 +0900

    Update config

--- aws-tools/list_regions_and_availability_zones.sh
+++ aws-tools/list_regions_and_availability_zones.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-for i in `aws ec2 describe-regions | jq -r '.Regions[].RegionName'`
-do
-	for j in `aws --region $i ec2 describe-availability-zones | jq -r '.AvailabilityZones[].ZoneName'`
-	do
-		echo $i:$j
-	done
-done
+aws ec2 describe-regions | jq -r '.Regions[].RegionName'
+
+#for i in `aws ec2 describe-regions | jq -r '.Regions[].RegionName'`
+#do
+#	for j in `aws --region $i ec2 describe-availability-zones | jq -r '.AvailabilityZones[].ZoneName'`
+#	do
+#		echo $i:$j
+#	done
+#done
