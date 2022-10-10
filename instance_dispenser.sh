aae10e9c928ed8617aa1e482e077ebb74c83532e
koya <kouyan120706@gmail.com>
2022-10-10 15:04:49 +0900

    Update global_config.txt

--- instance_dispenser.sh
+++ instance_dispenser.sh
@@ -10,5 +10,16 @@ for i in ${cluster_config[@]};
 do
     cloud=`echo $i | cut -d ',' -f 1`
     region=`echo $i | cut -d ',' -f 2`
-    cat $cloud/available-regions.txt | grep -i $region >> $cloud/my-region-list.txt
+    if [ $cloud="aws" ]; then
+        index=1
+    elif [ $cloud="azure" ]; then
+        index=2
+    elif [ $cloud="gcp" ]; then
+        index=3
+    else
+        echo not found cloud
+        exit 1
+    fi
+    region_name=`cat regions.csv | grep $region | cut -f $index`
+    cat $cloud/available-regions.txt | grep -i $region_name >> $cloud/my-region-list.txt
 done
