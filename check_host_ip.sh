1f143d29e8ce50a050c0ce43b2042f435eac3f5d
koya <kouyan120706@gmail.com>
2022-10-13 13:16:28 +0900

    Update check_hosts_ip

--- check_host_ip.sh
+++ check_host_ip.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+cd aws
+./list_instances_on_all_regions.sh | cut -d ":" -f 2,4 > my-hosts-list.txt
+cd ..
+
+cd azure
+./sbin/vm-ip.sh settings.sh > vm.ip
+cd ..
+
+cd gcp
+./vm-ip.sh > vm.ip
+cd ..
+
