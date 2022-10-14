c7034fb768ff7967df91ccc8bd059a9c102adf78
koya <kouyan120706@gmail.com>
2022-10-14 09:36:33 +0900

    Update setting.sh '

--- gcp/settings.sh
+++ gcp/settings.sh
@@ -8,6 +8,8 @@ machine_type=e2-medium
 # Set your private key path
 ssh_file_path=$ssh_pubkey_path
 
+# Vm user name
+vm_username=$HOME
 # "friendly name, region name"
 # "friendly name, region name"
 # "friendly name, region name"
