d17eafb557aa1e9982d84c446169fabee1399c8d
koya <kouyan120706@gmail.com>
2022-10-19 00:39:09 +0900

    parallelization create_cluster.sh delete_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -2,27 +2,35 @@
 source ./global_config.sh
 
 ./instance_dispenser.sh
-echo setup aws instances
-cd aws
 (
-./create_my_vpcs_on_all_regions.sh
-./list_ami_of_all_regions.sh
-./create_and_run_instances_on_all_regions.sh
-) >> aws.log 
-cd ..
+    cd aws
+    (
+        ./create_my_vpcs_on_all_regions.sh
+        ./list_ami_of_all_regions.sh
+        ./create_and_run_instances_on_all_regions.sh
+    ) >> aws.log 
+    cd ..
+) &
+pid1=${!}
 
-echo setup azure instances
-cd azure 
 (
-./sbin/create_cluster.sh settings.sh
-) >> azure.log 
-cd ..
+    cd azure 
+    (
+        ./sbin/create_cluster.sh settings.sh
+    ) >> azure.log
+    cd ..
+) &
+pid2=${!}
 
-echo setup gcp instances
-cd gcp
 (
-./create_all_instances.sh
-./set_firewall_rules.sh
-) >> gcp.log 
-cd ..
+    cd gcp
+    (
+    ./create_all_instances.sh
+    ./set_firewall_rules.sh
+    ) >> gcp.log 
+    cd ..
+) &
+pid3=${!}
 
+wait ${pid1} ${pid2} ${pid3}
+echo "Done create cluster"
--- delete_cluster.sh
+++ delete_cluster.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 source ./global_config.sh
 
-echo delete aws instances
-cd aws
 (
-./release_all_elastic_ips.sh
-./terminate_all_instances.sh
-./delete_my_vpcs_on_all_regions.sh
-) >> aws.log 
-cd ..
+    cd aws
+    (
+        ./release_all_elastic_ips.sh
+        ./terminate_all_instances.sh
+        ./delete_my_vpcs_on_all_regions.sh
+    ) >> aws.log 
+    cd ..
+) &
+pid1=${!}
 
-echo delete azure instances
-cd azure
 (
-./sbin/delete_cluster.sh settings.sh
-) >> azure.log 
-cd ..
+    cd azure
+    (
+        ./sbin/delete_cluster.sh settings.sh
+    ) >> azure.log 
+    cd ..
+) &
+pid2=${!}
 
-echo delete gcp instances
-cd gcp
 (
-./delete_all_instances.sh
-) >> gcp.log
-cd ..
+    cd gcp
+    (
+        ./delete_all_instances.sh
+    ) >> gcp.log
+    cd ..
+) &
+pid3=${!}
+
+wait ${pid1} ${pid2} ${pid3}
+echo "Done delete cluster"
