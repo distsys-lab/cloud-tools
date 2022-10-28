21b8c2d6c08b552784a5672372d4c9abc262a323
koya <koya@dsl.cs.tut.ac.jp>
2022-10-28 09:47:31 +0900

    Add stop_instance.sh

--- stop_instance.sh
+++ stop_instance.sh
@@ -6,16 +6,14 @@ if [ -z $1 ] || [ -z $2 ]; then
     echo "The arguments are 1:cloud name, 2:region name";
     exit 1
 fi
-echo Creating $1-$2 instance.
+echo Stoping $1-$2 instance.
 
 if [ $1 = "aws" ]; then
     cd $1
     source ./settings.sh
     region_name=`cat available-regions.txt | grep -i $2 | cut -d ":" -f 1`
-    friendly_name=`cat available-regions.txt | grep -i $2 | cut -d ":" -f 3`
-    ./create_my_vpc.sh $region_name $friendly_name
-    ami=`cat available-images.txt | grep -i $region_name | cut -d ":" -f 2`
-    ./run_instance.sh $region_name $friendly_name $ami
+    id=`./list_instances_on_all_regions.sh | grep $region_name | cut -d ":" -f 3`
+    ./stop_instance.sh $region_name $id
     cd ..
 elif [ $1 = "azure" ]; then
     cd $1
