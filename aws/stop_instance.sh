ae748a20e5d910c2d21a73477d087ddeb1d0f617
koya <koya@dsl.cs.tut.ac.jp>
2022-10-28 09:57:39 +0900

    Delete aws/stop_instance.sh and Update /stop_instance.sh

--- aws/stop_instance.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z $1 ] || [ -z $2 ]; then
-    echo "Error: Argument value is missing.";
-    echo "The arguments are 1:region name, 2:instance id";
-    exit 1
-fi
-aws ec2 --region $1 stop-instances --instance-ids $2
--- stop_instance.sh
+++ stop_instance.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
 source ./global_config.sh
 
-if [ -z $1 ] || [ -z $2 ]; then
-    echo "Error: Argument value is missing.";
-    echo "The arguments are 1:cloud name, 2:region name";
-    exit 1
-fi
-echo Stoping $1-$2 instance.
+cd aws 
+source ./settings.sh
+for i in `./list_instances_on_all_regions.sh`
+do
+ aws ec2 --region `echo $i | cut -d : -f 1` stop-instances --instance-ids `echo $i | cut -d : -f 3`
+done
+cd ..
 
-if [ $1 = "aws" ]; then
-    cd $1
-    source ./settings.sh
-    region_name=`cat available-regions.txt | grep -i $2 | cut -d ":" -f 1`
-    id=`./list_instances_on_all_regions.sh | grep $region_name | cut -d ":" -f 3`
-    ./stop_instance.sh $region_name $id
-    cd ..
-elif [ $1 = "azure" ]; then
-    cd $1
-    ./sbin/create_cluster.sh settings.sh $2
-    cd ..
-elif [ $1 = "gcp" ]; then
-    cd $1
-    source ./settings.sh
-    tag=`cat available-regions.txt | grep -i $2 | cut -d ":" -f 2`
-    zone=`cat available-regions.txt | grep -i $2 | cut -d ":" -f 1`
-    ./create_instance.sh $tag $zone $machine_type
-    cd ..
-fi
+cd azure
+
+cd ..
+
+cd gcp
+
+cd ..
